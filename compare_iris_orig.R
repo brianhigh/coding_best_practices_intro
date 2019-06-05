ace430a4c7935aa9a6a352c512e6f7bf0ee06eb4
Brian High <brianhigh@users.noreply.github.com>
2019-06-05 15:29:45 -0700

    Update compare_iris_orig.R

--- compare_iris_orig.R
+++ compare_iris_orig.R
@@ -13,11 +13,11 @@ base_URL<-"https://archive.ics.uci.edu/ml/machine-learning-databases/iris"
 UCI_iris_1<-read.csv(paste(base_URL,"iris.data",sep="/"),header=FALSE)
 names(UCI_iris_1)=names(iris)
 levels(UCI_iris_1$Species) <- tolower(gsub('^Iris-', '', levels(UCI_iris_1$Species)))
-write.csv("H:\\My Documents\\R Examples\\iris_compare\\iris_data.csv",row.names=FALSE)
+write.csv(UCI_iris_1, "H:\\My Documents\\R Examples\\iris_compare\\iris_data.csv",row.names=FALSE)
 UCI_iris_2<-read.csv(paste(base_URL,"bezdekIris.data",sep="/"),header=FALSE)
 names(UCI_iris_2)=names(iris)
 levels(UCI_iris_2$Species) <- tolower(gsub('^Iris-', '', levels(UCI_iris_2$Species)))
-write.csv("H:\\My Documents\\R Examples\\iris_compare\\iris_data.csv",row.names=FALSE)
+write.csv(UCI_iris_2, "H:\\My Documents\\R Examples\\iris_compare\\iris_data.csv",row.names=FALSE)
 
 #Compare the "iris" data sets against the one that comes with R.
 dim(UCI_iris_1)
