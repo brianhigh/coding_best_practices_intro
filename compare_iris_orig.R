d49e943b346045b56128aab6be8806a4c5534cb7
Brian High <brianhigh@users.noreply.github.com>
2019-06-05 15:28:34 -0700

    Update compare_iris_orig.R

--- compare_iris_orig.R
+++ compare_iris_orig.R
@@ -1,45 +1,47 @@
-# Of the two "iris" data sets available, which one comes standard with R?
+# ---------------------------------------------------------------------------
+# This is a sample script which needs to be cleaned up for readability, etc.
+# ---------------------------------------------------------------------------
 
-# This is the "iris" data set that comes standard with R.
+#Of the two "iris" data sets available at UCI, which one comes standard with R?
+
+#This is the "iris" data set that comes standard with R.
+#?iris
 data(iris)
 
-# Get the two alternative "iris" data sets and clean-up to match R's "iris".
-base_URL <-
-    "https://archive.ics.uci.edu/ml/machine-learning-databases/iris"
+#Get the two alternative "iris" data sets and clean-up to match R's "iris".
+base_URL<-"https://archive.ics.uci.edu/ml/machine-learning-databases/iris"
+UCI_iris_1<-read.csv(paste(base_URL,"iris.data",sep="/"),header=FALSE)
+names(UCI_iris_1)=names(iris)
+levels(UCI_iris_1$Species) <- tolower(gsub('^Iris-', '', levels(UCI_iris_1$Species)))
+write.csv("H:\\My Documents\\R Examples\\iris_compare\\iris_data.csv",row.names=FALSE)
+UCI_iris_2<-read.csv(paste(base_URL,"bezdekIris.data",sep="/"),header=FALSE)
+names(UCI_iris_2)=names(iris)
+levels(UCI_iris_2$Species) <- tolower(gsub('^Iris-', '', levels(UCI_iris_2$Species)))
+write.csv("H:\\My Documents\\R Examples\\iris_compare\\iris_data.csv",row.names=FALSE)
 
-read_data <- function(URL) {
-    df <- read.csv(URL, header = FALSE)
-    names(df) = names(iris)
-    levels(df$Species) <- tolower(gsub('^Iris-', '', levels(df$Species)))
-    df
-}
+#Compare the "iris" data sets against the one that comes with R.
+dim(UCI_iris_1)
+dim(UCI_iris_2)
+identical(UCI_iris_1, iris)
+identical(UCI_iris_2, iris)
 
-UCI_iris_1 <- read_data(paste(base_URL, "iris.data", sep = "/"))
-UCI_iris_2 <- read_data(paste(base_URL, "bezdekIris.data", sep = "/"))
-
-UCI_iris_2 <-
-    read.csv(paste(base_URL, "bezdekIris.data", sep = "/"), header = FALSE)
-names(UCI_iris_2) = names(iris)
-levels(UCI_iris_2$Species) <-
-    tolower(gsub('^Iris-', '', levels(UCI_iris_2$Species)))
-
-#Compare the "iris" data sets.
+#Which rows are different?
 
 #install.packages("compare")
 library(compare)
 
 if (!identical(UCI_iris_1, UCI_iris_2)) {
     is.equal <- do.call(rbind, lapply(1:nrow(UCI_iris_1), function(x) {
-        res <- compareEqual(UCI_iris_1[x, ], UCI_iris_2[x, ])
-        res.df <- as.data.frame(t(res$detailedResult))
-        res.df$x <- x
-        if (!res$result) return(res.df)
-    }))
+                    res <- compareEqual(UCI_iris_1[x, ], UCI_iris_2[x, ])
+                    res.df <- as.data.frame(t(res$detailedResult))
+                    res.df$x <- x
+                    if (!res$result == TRUE) return(res.df)
+                }))
     print(is.equal)
     print(UCI_iris_1[is.equal$x, ])
     print(UCI_iris_2[is.equal$x, ])
 }
 
-identical(UCI_iris_1, iris)
-identical(UCI_iris_2, iris)
 
+# Homework: Do the same exercise in Python, comparing to "iris" in scikit-learn.
+# See: https://github.com/scikit-learn/scikit-learn/issues/10550
